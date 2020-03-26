ec013954232f68314843aee6b8b4084003dfbbe6
TheHarshShow <harshmotwanimail@gmail.com>
2020-03-26 15:53:22 +0530

    Created request and category models using geodjango and created means to get current location

--- geoLocate/models.py
+++ geoLocate/models.py
@@ -1,3 +1,29 @@
-from django.db import models
+from django.contrib.gis.db import models
 
 # Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+class Request(models.Model):
+
+    #details of person requesting
+    requestor=models.CharField(max_length=100)
+    location = models.PointField()
+    address = models.CharField(max_length=100)
+    city = models.CharField(max_length=50)
+
+    #requirement details
+    requirement = models.CharField(max_length=100)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    priority = models.IntegerField(default=0);
+    created = models.DateTimeField(auto_now_add=True)
+
+    #whether address/current location is shared or not
+    addressAllowed = models.BooleanField()
+
+    def __str__(self):
+        return self.requestor+str(self.created);
--- geoLocate/views.py
+++ geoLocate/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect, HttpResponse
 
 # Create your views here.
+
+def main_page(request):
+    return render(request, 'main_page.html')
